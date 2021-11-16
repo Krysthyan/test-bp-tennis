33c5e1c0be52303d43513704f87a39e13925c9ac
Cristhian <cristhian.bladimir.hernandez@gmail.com>
2021-11-16 18:20:54 -0500

    feat: mapping points

---
+++ src/main/ScoreMapper.java
@@ -0,0 +1,15 @@
+package main;
+
+public class ScoreMapper {
+
+    public String mapScore(int score) {
+        switch (score) {
+            case 0: return "Love";
+            case 1: return "Fifteen";
+            case 2: return "Thirty";
+            case 3: return "Forty";
+        }
+        throw new IllegalArgumentException("__Score: " + score + "___");
+    }
+
+}
